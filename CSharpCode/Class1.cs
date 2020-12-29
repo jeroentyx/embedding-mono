454078bb3d86a030139587a460532ee29bfaaa5f
Jeroen <49836368+jeroentyx@users.noreply.github.com>
2020-12-29 15:13:09 +0800

    first commit

---
+++ CSharpCode/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace CSharpCode
+{
+    public class Class1
+    {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static void PrintMethod(string msg);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This main is being called from cpp");
+            PrintMethod("Im Executing this code from CSharp!");
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <mono\jit\jit.h>
+#include <mono\metadata\assembly.h>
+
+
+
+void PrintMethod(MonoString* string)
+{
+	char* cppString = mono_string_to_utf8(string);
+	std::cout << cppString;
+	mono_free(cppString);
+}
+
+int main()
+{
+	mono_set_dirs("Mono\\lib", "Mono\\etc");
+	MonoDomain* domain = mono_jit_init("CSharp_Domain");
+	MonoAssembly* csharpAssembly =
+		mono_domain_assembly_open(domain, "CSharpCode\\bin\\Debug\\netstandard2.0\\CSharpCode.dll");
+	if (!csharpAssembly)
+	{
+		return -1;
+	}
+
+	//Set up internal calls
+	mono_add_internal_call("CSharpCode.Class1::PrintMethod", &PrintMethod);
+
+	int argc = 1;
+	char* argv[1] = { (char*)"CSharp" };
+
+	mono_jit_exec(domain, csharpAssembly, argc, argv);
+
+	return 0;
+}
